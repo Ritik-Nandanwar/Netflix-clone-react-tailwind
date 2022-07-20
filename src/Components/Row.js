57037a667dfa51a104002f3e00fa2e6a9bfcab31
Ritik Nandanwar <nandanwarritik10@gmail.com>
2022-07-21 00:54:49 +0530

    added comment for flexbox bug solution whitespace-nowrap(tailwind class)

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -15,6 +15,8 @@ function Row({ title, URL }) {
   return (
     <>
       <h1 className="text-red-600 font-semibold flex text-2xl py-2">{title}</h1>
+
+      {/* whitespace-nowrap => keeps all the images in one row , so now we can just manipulate the scroll behaviour ,NO NEED TO USE FLEXBOX */}
       <div className="w-full h-full overflow-x-scroll no-scrollbar whitespace-nowrap scroll-smooth scrollbar-hide relative space-x-4">
         {movies &&
           movies.map((item, id) => <Movie key={id} item={item} id={id} />)}
